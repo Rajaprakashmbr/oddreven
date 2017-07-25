27bc7262922de0bd6b770b96713414355a77eb5b
Rajaprakashmbr <rajaprakashmbr1996@gmail.com>
2017-07-25 09:44:28 +0530

    Create odd.cpp

---
+++ odd.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter the number:\n";
+cin>>n;
+if(n>0)
+{
+if(n%2==0)
+{
+cout<<"the number is even";
+}
+else
+{
+cout<<"the number is odd";
+}
+}
+else
+{
+cout<<"enter the valid number";
+}
+return 0;
+}
